77aa40e470264db5bb825553e5ece080e1a85a95
Rock <xiu43317@gmail.com>
2024-01-24 18:03:18 +0800

    git init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ data }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: {}
+    }
+  },
+  mounted() {
+    console.log(import.meta.env.VITE_TEXT)
+    this.$http.get(import.meta.env.VITE_PATH).then((res) => {
+      console.log(res)
+      this.data = res.data
+    })
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
